1c8ebb21a4039554a3a2a6dc336f064f937cc4d0
juha3 <fds90fjs@dddd.invalid>
2025-02-05 23:56:35 +0200

    update

--- src/main.c
+++ src/main.c
@@ -34,18 +34,7 @@
 #define UART_BUSY (1 << 0)
 #define UART_DATA_READY (1 << 1)
 #define UART_OVERFLOW (1 << 2)
-/*
-	GPIO 
 
-     DATA0 R/W 0x0300 3000
-     DATA1 R/W 0x0300 3004
-     PIN0  R   0x0300 3010
-     PIN1  R   0x0300 3014
-     DIR0  R/W 0x0300 3030
-     DIR1  R/W 0x0300 3034
-	.port0({sw, led}),
-	.port1({sevenseg, anode, btnc, btnu, btnr, btnl, btnd}),
- */
 #define SEVEN_SEG_PINS (0xfff << 5)
 
 #define LEDS_ALL 0xff
@@ -79,148 +68,15 @@
 
 #define NUMBERS {ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE}
 
-/*
-
-
-     aa 
-    f  b
-    f  b
-     gg 
-    e  c 
-    e  c 
-     dd  p 
-
-*/
-
- 
-//#define REG_TIMER *((volatile uint32_t*)(0x80000000))
-//#define REG_FINISH *((volatile uint32_t*)(0xffc))
-
-//#define UART_DATA *((volatile uint32_t*)(0xc0000000))
-//#define UART_STATUS *((volatile uint32_t*)(0xc0000004))
-//#define UART_CTRL *((volatile uint32_t*)(0xc0000008))
-//
-//#define UART_DATA_TX_BUSY (1 << 0)
-//#define UART_DATA_RDY (1 << 1)
-//
-//#define UART_CTRL_IEN (1 << 0)
-
-//volatile uint32_t flag;
-//volatile uint8_t uart_byte;
-//volatile uint8_t uart_data_rdy;
-
-/* general IRQ handler */
-//void irq_handler()
-//{
-//	REG_TIMER += 1000000;
-//
-//	if (UART_STATUS & UART_DATA_RDY) {
-//		uart_byte = UART_DATA;
-//		uart_data_rdy = 1;
-//	}
-//}
-
-//void dbg_print_str(char *s)
-//{
-//	(void)s;
-//#ifdef SIM
-//	while(1) {
-//		if (*s == 0) break;
-//		REG_DBG = *s++;
-//	}
-//#endif
-//}
-
-//void dbg_print_int(int32_t v)
-//{
-//	int v1 = 1000000000;
-//	int v2;
-//	char b[2] = {0};
-//
-//	if (v == 0) {
-//		dbg_print_str("0");
-//		return;
-//	}
-//	while(1) {
-//		v2 = v / v1;
-//		if (b[0] || v2 || v1 <= 10) {
-//			b[0] = 0x30 + v2;
-//			dbg_print_str(b); 
-//		}
-//		v = v % v1;
-//		v1 = v1 / 10;
-//		if (v1 == 0) break;
-//	}
-//}
-
-//uint32_t set_timer(uint32_t t);
 volatile uint16_t c;
 
-
-int loop()
-{
-//	uint32_t v;
-
-	//char b[2] = {0};
-	//int i;
-	//if (UART_STATUS == 0x00) while(1);	
-
-//	for (c = 0; ; c++) {
-//		REG_LED = (c & (1 << 13)) ? 1 : 0;
-////		if (c == (1 << 12)) 
-////			REG_UART = 'a';
-//	}
-//	REG_UART = 'x';
-	//UART_DATA = 'x';
-	//while((UART_STATUS & UART_DATA_TX_BUSY));
-	
-//	while(1) {
-//		v = UART_STATUS;
-//		if ((v & UART_DATA_RDY)) {
-//			v = UART_DATA;
-//			UART_DATA = v & 0xff;
-//			break;
-//		}
-//	}
-//	while(uart_data_rdy == 0);
-//	uart_data_rdy = 0;
-//	v = uart_byte;
-//	UART_DATA = v & 0xff;
-//
-//	while((UART_STATUS & UART_DATA_TX_BUSY));
-//	//REG_UART = 0xbb;
-//	//for (c = 0; c < 100; c++) {
-//	//	REG_LED = (c & (1 << 4)) ? 1 : 0;
-//	//}
-//	v &= 0xff;
-//	if (v == '1') REG_LED = 1;
-//	if (v == '0') REG_LED = 0;
-
-//	for (i = 0; i < 10; i++) {
-//		b[0] = i + 0x30;
-//		dbg_print_str(b);
-//	}
-//	dbg_print_str("\n");
-//	dbg_print_int(1500);
-//	dbg_print_str("\n");
-//	i = set_timer(100);
-//	while(flag == 0);
-//	dbg_print_str("done\n");
-
-//	while(1);
-//	return 0;
-	return 0;
-}
 
 
 int main()
 {
-    //int i;
-    //int n; 
     uint32_t v;
 	uint32_t cnt = 0;
     uint32_t n[] = {ZERO, ONE, TWO, THREE};
-    //n = (REG_DBG0 & SIM) ? 10 : 100000000;
     
     REG_GPIO_DIR1 = SEVEN_SEG_PINS;
     REG_GPIO_DATA1 = FOUR | ANODE1;
@@ -230,24 +86,13 @@ int main()
 
     REG_RNG_CTRL = RNG_EN;
     while(1) {
-        v = REG_GPIO_PIN1; // btnd to toggle led0
-        //if (v & 1) REG_GPIO_DATA0 |= 1;
-        //else REG_GPIO_DATA0 &= ~(0x1);
+        v = REG_GPIO_PIN1;
         if (v & 1) {
             while(!(REG_RNG_STATUS & RNG_RDY));
             cnt = REG_RNG_OUT & 0x3;
-            //cnt = (cnt + 1);
-            //if (cnt == 4) cnt = 0;
         }
         REG_GPIO_DATA1 = n[cnt] | ANODE1;
     }
-    //for (i = 0; i < n; i++) {
-    //    while(!(REG_RNG_STATUS & RNG_RDY));
-    //    while(REG_UART_STATUS & UART_BUSY);
-    //    REG_UART_TX_DATA = REG_RNG_OUT;
-    //}
-    //    
-    //while(REG_UART_STATUS & UART_BUSY);
 
     REG_DBG0 = 0xaa;
     while(1);
